8861a811b2d0156d48883b803e2d3eb4a991d9df
Olexander Bryl <edlrian814@gmail.com>
2023-11-06 18:12:57 +0200

    Add models Train & TrainType

--- station/models.py
+++ station/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class TrainType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Train(models.Model):
+    name = models.CharField(max_length=255)
+    cargo_num = models.IntegerField()
+    places_in_cargo = models.IntegerField()
+    train_type = models.ForeignKey(
+        "TrainType",
+        on_delete=models.CASCADE,
+        related_name="trains"
+    )
+
+    class Meta:
+        ordering = ["name"]
+
+    @property
+    def capacity(self) -> int:
+        return self.cargo_num * self.places_in_cargo
+
+    def __str__(self):
+        return f"{self.name} ({self.train_type}, {self.capacity} places)"
+
+
+
+
+
