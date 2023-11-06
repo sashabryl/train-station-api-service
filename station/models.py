6f23bb205a6f6b106b12f95b9ce5784eaf48ef93
Olexander Bryl <edlrian814@gmail.com>
2023-11-06 19:23:01 +0200

    Add models Crew & Station & Route & Journey & Order & Ticket

--- station/models.py
+++ station/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -32,6 +33,97 @@ class Train(models.Model):
         return f"{self.name} ({self.train_type}, {self.capacity} places)"
 
 
+class Station(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    latitude = models.FloatField()
+    longtitude = models.FloatField()
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} ({self.latitude}, {self.longtitude})"
 
 
+class Route(models.Model):
+    source = models.ForeignKey(
+        "Station",
+        on_delete=models.CASCADE,
+        related_name="routes_from"
+    )
+    destination = models.ForeignKey(
+        "Station",
+        on_delete=models.CASCADE,
+        related_name="routes_to"
+    )
+    distance = models.IntegerField()
 
+    class Meta:
+        ordering = ["-distance"]
+
+    def __str__(self):
+        return f"{self.source} - {self.destination}"
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Journey(models.Model):
+    route = models.ForeignKey(
+        "Route",
+        on_delete=models.CASCADE,
+        related_name="journeys"
+    )
+    train = models.ForeignKey(
+        "Train",
+        on_delete=models.CASCADE,
+        related_name="journeys"
+    )
+    departure_time = models.DateTimeField()
+    crew_members = models.ManyToManyField(
+        "Crew", related_name="journeys"
+    )
+
+    class Meta:
+        ordering = ["-departure_time"]
+
+    def __str__(self):
+        return f"{self.route}, {self.departure_time}"
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="orders"
+    )
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class Ticket(models.Model):
+    cargo = models.IntegerField()
+    seat = models.IntegerField()
+    journey = models.ForeignKey(
+        "Journey",
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+    order = models.ForeignKey(
+        "Order",
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+
+    def __str__(self):
+        return (
+            f"{self.journey.route}-"
+            f", departure: {self.journey.departure.time}"
+        )
--- train_station_service/settings.py
+++ train_station_service/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "station",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -102,6 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "user.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
